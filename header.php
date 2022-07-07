a8b453b717a5b3603d037c4dc298f50c267b32ba
I.Spataru <iulion08@gmail.com>
2022-07-07 20:40:47 +0100

    Media query and content changes

--- header.php
+++ header.php
@@ -5,7 +5,9 @@
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="main.css">
+	<link rel="stylesheet" href="css/main.css" media="all">
+    <link rel="stylesheet" href="css/medium.css" media="(min-width: 64em)" />
+    <link rel="stylesheet" href="css/large.css" media="(min-width: 90em)" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
 		integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
 		crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -19,9 +21,8 @@
 	<header>
 		<nav>
 			<ul class="topnav">
-				<li><a href="#" class="active"><img class="logo" src="media/logo.svg" alt="Logo"></a></li>
+				<li><a href="#" class="active"><img class="logo" src="media/logo.svg" alt="Logo"/></a></li>
 				<div id="myLinks">
-					<a href="#">Menu Item</a>
 					<a href="#">Menu Item</a>
 					<a href="#">Menu Item</a>
 					<a href="#">Menu Item</a>
@@ -29,6 +30,7 @@
 					<div class="topnav__extra">
 						<a href="#"><i class="fa-solid fa-magnifying-glass fa-xl"></i></a>
 						<a href="#"><i class="fa-solid fa-user fa-xl"></i></a>
+                        <a href="#" class="topnav__button">Get in touch</a>
 					</div>
 				</div>
 				<a href="javascript:void(0);" class="icon" onclick="mobileMenu()">
